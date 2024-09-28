04871604e9e4b97dcb14350599fc446eab7ceff6
Braulio Oliveira <brauliobo@gmail.com>
2024-09-28 12:56:12 -0300

    Use env DEBUG to enable debug

--- bindp.c
+++ bindp.c
@@ -55,7 +55,7 @@
 #include <errno.h>
 #include <arpa/inet.h>
 
-int debug_enabled = 1 ;
+int debug_enabled = 0;
 
 int (*real_bind)(int, const struct sockaddr *, socklen_t);
 int (*real_connect)(int, const struct sockaddr *, socklen_t);
@@ -70,6 +70,9 @@ unsigned int ip_transparent = 0;
 
 void _init (void) {
     const char *err;
+
+    if (getenv("DEBUG") != NULL)
+        debug_enabled = 1;
 
     real_bind = dlsym (RTLD_NEXT, "bind");
     if ((err = dlerror ()) != NULL) {
